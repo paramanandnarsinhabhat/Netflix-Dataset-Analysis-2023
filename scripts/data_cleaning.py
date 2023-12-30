1b52a871a87c5912ef8300e3295c557d8cce96a0
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-30 19:18:48 +0530

    FUnction to detect the outliers in rating column

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -4,7 +4,7 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from utils.data_utils import load_data,convert_data_types,standardize_and_clean_data,handle_missing_values,validate_data
+from utils.data_utils import load_data,convert_data_types,standardize_and_clean_data,handle_missing_values,validate_data,extract_date_components,detect_outliers
 
 file_path = '/Users/paramanandbhat/Downloads/total_netflix_2023 new.csv'
 
@@ -23,4 +23,8 @@ netflix_df = validate_data(netflix_df)
 
 #Step 5 : Extract the date components to Extract Additional Information Function
 netflix_df = extract_date_components(netflix_df)
+
+#Step 6 : Outlier Detection Function
+
+netflix_df = detect_outliers(netflix_df, 'Rating')
 
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -39,5 +39,12 @@ def extract_date_components(df):
     df['Day'] = df['Release Date'].dt.day
     return df
 
+def detect_outliers(df, column_name):
+    Q1 = df[column_name].quantile(0.25)
+    Q3 = df[column_name].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return df[(df[column_name] >= lower_bound) & (df[column_name] <= upper_bound)]
 
 
