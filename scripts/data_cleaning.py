56bb6ccedb1740f55dfcf981d90e7b38f32d8a99
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-30 18:38:27 +0530

    Adding function to parse genre

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -9,9 +9,10 @@ from utils.data_utils import load_data,convert_data_types
 file_path = '/Users/paramanandbhat/Downloads/total_netflix_2023 new.csv'
 
 netflix_df = load_data(file_path)
+# Step 1: Convert Data Types
 netflix_df = convert_data_types(netflix_df)
 
-# Step 1: Convert Data Types
+
 
 # Convert 'Release Date' to datetime
 netflix_df['Release Date'] = pd.to_datetime(netflix_df['Release Date'], errors='coerce')
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+def parse_genre(genre_str):
+    genre_str = genre_str.strip("[]")
+    return [genre.strip("' ") for genre in genre_str.split(",")]
+
 def load_data(file_path):
     """Load data from a CSV file into a DataFrame."""
     return pd.read_csv(file_path)
@@ -11,6 +15,12 @@ def convert_data_types(df):
     df['Rating'] = pd.to_numeric(df['Rating'], errors='coerce')
     return df
 
+def standardize_and_clean_data(df):
+    df['Available Globally?'] = df['Available Globally?'].map({'Yes': True, 'No': False})
+    df['Genre'] = df['Genre'].apply(parse_genre)
+    df['Key Words'] = df['Key Words'].str.strip()
+    df['Description'] = df['Description'].str.strip()
+    return df
 
 
 
