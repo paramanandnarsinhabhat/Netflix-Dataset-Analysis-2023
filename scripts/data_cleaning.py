e8e7c01acd71a3ea56cbc6184b79e1c621a053e5
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-30 19:26:22 +0530

    extract date components and handle missing values

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -15,14 +15,15 @@ netflix_df = convert_data_types(netflix_df)
 # Step 2: Standardize Text and Categorical Data
 netflix_df = standardize_and_clean_data(netflix_df)
 
-#Step 3 : Handle missing values
+
+#Step 3 : Extract the date components to Extract Additional Information Function
+netflix_df = extract_date_components(netflix_df)
+#Step 4 : Handle missing values
 netflix_df = handle_missing_values(netflix_df)
 
-#Step 4: Validate Data Function
+#Step 5: Validate Data Function
 netflix_df = validate_data(netflix_df)
 
-#Step 5 : Extract the date components to Extract Additional Information Function
-netflix_df = extract_date_components(netflix_df)
 
 #Step 6 : Outlier Detection Function
 
